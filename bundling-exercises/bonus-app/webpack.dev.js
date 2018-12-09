54b293e330386b9c35e1c7ee19ea0553e29b0c46
Aarón Suárez <aaron.suarez@outlook.com>
2018-12-09 19:47:25 +0000

    added webpack bundle analyzer

--- bundling-exercises/bonus-app/webpack.dev.js
+++ bundling-exercises/bonus-app/webpack.dev.js
@@ -1,7 +1,13 @@
 const merge = require('webpack-merge');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const common = require('./webpack.common.js');
 
 module.exports = env => merge(common(env), {
     mode: 'development',
     devtool: 'inline-source-map',
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerPort: 8989,
+        }),
+    ],
 });
