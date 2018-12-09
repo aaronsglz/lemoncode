6537535ad6c2967b6c62a74c51a04269c5f84434
Aarón Suárez <aaron.suarez@outlook.com>
2018-12-09 19:30:47 +0000

    added enviroment variables

--- bundling-exercises/bonus-app/webpack.common.js
+++ bundling-exercises/bonus-app/webpack.common.js
@@ -4,86 +4,91 @@ var webpack = require('webpack');
 var path = require('path');
 var basePath = __dirname;
 
-module.exports = {
-	context: path.join(basePath, 'src'),
-	resolve: {
-		extensions: ['.js', '.ts', '.tsx']
-	},
-	entry: {
-		app: './index.tsx',
-		appStyles: [
-			'./scss/main.scss',
-		],
-	},
-	output: {
-		filename: 'js/[name].[chunkhash].js',
-	},
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendor: {
-					chunks: 'initial',
-					name: 'vendor',
-					test: 'vendor',
-					enforce: true
-				},
+module.exports = env => {
+	
+	console.log('\nCUSTOM_MODE: ' + env.customMode + '\n\n');
+
+	return {
+		context: path.join(basePath, 'src'),
+		resolve: {
+			extensions: ['.js', '.ts', '.tsx']
+		},
+		entry: {
+			app: './index.tsx',
+			appStyles: [
+				'./scss/main.scss',
+			],
+		},
+		output: {
+			filename: 'js/[name].[chunkhash].js',
+		},
+		optimization: {
+			splitChunks: {
+				cacheGroups: {
+					vendor: {
+						chunks: 'initial',
+						name: 'vendor',
+						test: 'vendor',
+						enforce: true
+					},
+				}
 			}
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(ts|tsx)$/,
-				exclude: /node_modules/,
-				loader: 'awesome-typescript-loader',
-				options: {
-					useBabel: true,
-					"babelCore": "@babel/core", // needed for Babel v7
-				},
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					"css-loader",
-					"sass-loader",
-				]
-			},
-			{
-				test: /\.css$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					"css-loader"
-				]
-			},
-			{
-				test: /\.(png|jpg)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'url-loader',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(ts|tsx)$/,
+					exclude: /node_modules/,
+					loader: 'awesome-typescript-loader',
 					options: {
-						limit: 5000,
-						name: './images/[hash].[name].[ext]',
+						useBabel: true,
+						"babelCore": "@babel/core", // needed for Babel v7
 					},
 				},
-			},
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader',
+				},
+				{
+					test: /\.scss$/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						"css-loader",
+						"sass-loader",
+					]
+				},
+				{
+					test: /\.css$/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						"css-loader"
+					]
+				},
+				{
+					test: /\.(png|jpg)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'url-loader',
+						options: {
+							limit: 5000,
+							name: './images/[hash].[name].[ext]',
+						},
+					},
+				},
+			],
+		},
+		plugins: [
+			//Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+			new HtmlWebpackPlugin({
+				filename: 'index.html', //Name of file in ./dist/
+				template: 'index.html', //Name of template in ./src
+				hash: true,
+			}),
+			new MiniCssExtractPlugin({
+				filename: "css/[name].css",
+				chunkFilename: "css/[id].css",
+			}),
 		],
-	},
-	plugins: [
-		//Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
-		new HtmlWebpackPlugin({
-			filename: 'index.html', //Name of file in ./dist/
-			template: 'index.html', //Name of template in ./src
-			hash: true,
-		}),
-		new MiniCssExtractPlugin({
-			filename: "css/[name].css",
-			chunkFilename: "css/[id].css",
-		}),
-	],
+	}
 };
--- bundling-exercises/bonus-app/webpack.prod.js
+++ bundling-exercises/bonus-app/webpack.prod.js
@@ -1,8 +1,8 @@
 const merge = require('webpack-merge');
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const common = require('./webpack.common.js');
 
-module.exports = merge(common, {
+module.exports = env =>  merge(common(env), {
     mode: 'production',
     optimization: {
         minimizer: [
