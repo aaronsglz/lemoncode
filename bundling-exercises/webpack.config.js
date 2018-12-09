b4e47642c56af1c9bc69ac9e5b280a8d7c5069a4
Aarón Suárez <aaron.suarez@outlook.com>
2018-12-09 12:18:14 +0000

    bundling con webpack

---
+++ bundling-exercises/webpack.config.js
@@ -0,0 +1,61 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var webpack = require('webpack');
+var path = require('path');
+var basePath = __dirname;
+
+module.exports = {
+	context: path.join(basePath, 'src'),
+	resolve: {
+		extensions: ['.js']
+	},
+	entry: {
+		app: './js/index.js',
+		appStyles: [
+			'./css/main.css',
+		],
+	},
+	output: {
+		filename: 'js/[name].[chunkhash].js',
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					chunks: 'initial',
+					name: 'vendor',
+					test: 'vendor',
+					enforce: true
+				},
+			}
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader"
+				]
+			},
+		],
+	},
+	plugins: [
+		//Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+		new HtmlWebpackPlugin({
+			filename: 'index.html', //Name of file in ./dist/
+			template: 'index.html', //Name of template in ./src
+			hash: true,
+		}),
+		new MiniCssExtractPlugin({
+			filename: "css/[name].css",
+			chunkFilename: "css/[id].css"
+		}),
+	],
+};
